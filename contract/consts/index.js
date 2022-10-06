32abb8a28ba5daeb2d236d2665bbe68ab80c73e1
Pedro Santana <pedrosantana@duck.com>
2022-10-06 16:09:56 +0100

    app init

---
+++ contract/consts/index.js
@@ -0,0 +1,4 @@
+const WHITELIST_CONTRACT_ADDRESS = "0xce9598D28576878905902c4CdD179017e8D6377a";
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
